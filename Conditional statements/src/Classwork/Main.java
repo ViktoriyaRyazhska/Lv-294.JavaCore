7f24b987039664eaab6a7eaa8e4d9445fef75fc1
MaxymSydoriv <maxym19998@gmail.com>
2018-02-07 20:54:21 +0200

    Task 4

--- Conditional statements/src/Classwork/Main.java
+++ Conditional statements/src/Classwork/Main.java
@@ -10,8 +10,10 @@ public class Main {
 		task2.doTask();*/
 		
 		//Third Task doesn't work.I will try to fix it or ask you what the problem is
-		ThirdTask task3 = new ThirdTask();
-		task3.doTask();
+		//ThirdTask task3 = new ThirdTask();
+		//task3.doTask();
+		FourthTask task4 = new FourthTask();
+		task4.doTask(new Product("Table",50d,10),new Product("Wardrobe", 100d,5),new Product("Chair",25d,25),new Product("Mirror", 8d,7));
 		
 		
 	}

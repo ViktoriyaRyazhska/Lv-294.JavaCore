1bd13bad9a40d83d9f980657f6bf4c49c5e29f65
Ira <ira.shturyn@gmail.com>
2018-02-08 22:20:16 +0200

    corrected years& Br

--- HomeworkTwo/src/Person.java
+++ HomeworkTwo/src/Person.java
@@ -1,18 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.time.Year;
 
 public class Person {
     private String name;
     private int birthYear;
-    private int year;
-
-    /*Додала поле private int year, щоб програма була більш універсальною,
-     бо, наприклад, у випадку, коли людина хоче порахувати скільки їй буде років
-     через десять років, чи скільки було 5 років тому, програма не обчислить,
-     оскільки буде рахувати лише поточний рік*/
-
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public Person() {
     }
@@ -26,33 +18,27 @@ public class Person {
         this.name = name;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
-
     public void setBirthYear(int birthYear) {
         this.birthYear = birthYear;
     }
 
     public int age() {
-        return year - birthYear;
+        return Year.now().getValue() - birthYear;
     }
 
     public void changeName(String name) {
         this.name = name;
     }
 
-    public void input() throws IOException {
+    public void input(BufferedReader br) throws IOException {
         System.out.println("Enter your name");
         name = br.readLine();
         System.out.println("Enter your birth year");
         birthYear = Integer.parseInt(br.readLine());
-        System.out.println("Enter current year");
-        year = Integer.parseInt(br.readLine());
     }
 
     public void output() {
-        System.out.println("Name is: " + name + ". Person was born in " + birthYear + ". Now is " + year + " year. Age is " + age() + ".");
+        System.out.println("Name is: " + name + ". Person was born in " + birthYear + ". Age is " + age() + ".");
     }
 
 }
--- HomeworkTwo/src/Runner.java
+++ HomeworkTwo/src/Runner.java
@@ -1,34 +1,34 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class Runner {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("--1--");
         Person person = new Person();
-        person.input();
+        person.input(br);
         person.output();
 
         System.out.println("--2--");
         Person person1 = new Person("Levko", 1976);
-        person1.setYear(2000);
-        person1.output();
+         person1.output();
 
         System.out.println("--3--");
         Person person2 = new Person();
         person2.setName("Anna");
         person2.setBirthYear(1995);
-        person2.setYear(2018);
         person2.changeName("Vira");
         person2.output();
 
         System.out.println("--4--");
         Person person3 = new Person();
-        person3.input();
+        person3.input(br);
         person3.output();
 
         System.out.println("--5--");
         Person person4 = new Person("Ivan", 2001);
-        person4.setYear(2046);
         person4.output();
     }
 }

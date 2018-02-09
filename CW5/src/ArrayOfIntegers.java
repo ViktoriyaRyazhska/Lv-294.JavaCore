188efe1eda4d60ad075852ce7b9477c48330eaa8
User <User@WIN-7V945D2C>
2018-02-09 22:48:33 +0200

    cw5

--- CW5/src/ArrayOfIntegers.java
+++ CW5/src/ArrayOfIntegers.java
@@ -15,33 +15,23 @@ public class ArrayOfIntegers {
 	public static void main(String[] args) throws IOException {
 		
 		System.out.println("Please, input ten integers");
-		
-	    BufferedReader br = new BufferedReader(
-	    		new InputStreamReader(System.in));
-	    
-	
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   
 		int array[ ]  = new int[10];
-		
 		int max=0;
+		int posamo=0;
+		int negamo=0;
+		int possum=0;
+		int negsum=0;
 		
 		for(int i = 0; i < 10; i++){
-			
 			
 			array[i] = Integer.parseInt(br.readLine());
 			
 			if ((i==0)||(array[i]>max)) {
 				max = array[i];
 			}
-			
-			System.out.println("The biggest number is" + max);			
-			
-			
-		int posamo=0;
-		int negamo=0;
-		int possum=0;
-		int negsum=0;
 	
-
 			if (array[i]>0) {
 				posamo++;
 				possum+=array[i];
@@ -49,10 +39,11 @@ public class ArrayOfIntegers {
 				negamo++;
 				negsum+=array[i];
 			}
-				
-			System.out.println("The sum of POSitive numbers:" + possum);
-			System.out.println("The sum of NEGative numbers:" + negsum);
 			
+		}
+			System.out.println("The biggest number is " + max);			
+			System.out.println("The sum of POSitive numbers:" + possum);
+			System.out.println("The sum of NEGative numbers:" + negsum);		
 			System.out.println("Amount of POSitive numbers:" + posamo);
 			System.out.println("Amount of NEGative numbers:" + negamo);
 			
@@ -62,16 +53,8 @@ public class ArrayOfIntegers {
 				else if (posamo==negamo)
 					System.out.println("There are draw amount of positive and negative values");
 					else
-						System.out.println("There are more positive values");	
-				
-		
+						System.out.println("There are more positive values");		
 			
-			
-		}
-
-		
-		System.out.println(max);	
-		
 	}
 
 }

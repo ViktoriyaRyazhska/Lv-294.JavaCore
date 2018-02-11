86705db9ccdb2760a87c7e5394a49387010c9649
User <User@WIN-7V945D2C>
2018-02-11 22:47:41 +0200

    JUnit

---
+++ CW4/JUnit/CalcTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class CalcTest {
+    Calc calc = new Calc();
+
+    @Test
+    public void testAdd() { assertTrue(calc.add(1, 5) == 6);}
+
+    @Test
+    public void testDivPositive() {
+        int actual = 4;
+        int expected = calc.div(9, 2);
+        assertEquals(actual, expected);
+    }
+    
+    @Test(expected = Exception.class)
+    public void testDivZero() {
+        int actual = calc.div(23, 0);
+    } 
+}
+
---
+++ CW4/src/Calc.java
@@ -0,0 +1,10 @@
+
+public class Calc {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int div(int a, int b){
+        return a / b;
+    }
+}

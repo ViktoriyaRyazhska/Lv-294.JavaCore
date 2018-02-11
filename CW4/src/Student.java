28388c3b12993d2315b161181c1827932e4dd487
User <User@WIN-7V945D2C>
2018-02-11 20:52:51 +0200

    CW4 Obj comparision

---
+++ CW4/src/Student.java
@@ -0,0 +1,49 @@
+
+public class Student {
+	
+	private String name;
+	private int age;
+	
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result 
+			+ ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Student other = (Student) obj;
+		if (age != other.age) return false;
+		if (name == null) {
+			if (other.name != null) return false;
+		} else if (!name.equals(other.name)) return false;
+		return true;
+	}
+	
+	public static void main(String[] args) {
+
+		Student student1 = new Student("Ira", 25);
+		Student student2 = new Student("Ira", 25);
+		System.out.println(student1 == student2);
+		System.out.println(student1.equals(student2));
+		System.out.println(student1.hashCode());
+		System.out.println(student2.hashCode());
+
+
+
+
+
+	}
+}

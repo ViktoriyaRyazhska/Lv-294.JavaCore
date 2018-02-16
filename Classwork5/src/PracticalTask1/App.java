36deab26c712a7072ba74d217044e9316cf3a00c
Ira <ira.shturyn@gmail.com>
2018-02-16 14:58:44 +0200

    finished clw5

--- Classwork5/src/PracticalTask1/App.java
+++ Classwork5/src/PracticalTask1/App.java
@@ -1,57 +1,45 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.LinkedList;
+package PracticalTask1;
 
-/*
- * Declare collection myCollection of 10 integers and fill it (from the console or random).
-Find and save in list newCollection all positions of element more than 5 in the collection. Print newCollection
-Remove from collection myCollection elements, which are greater then 20. Print result
-Insert elements 1, -3, -4 in positions 2, 8, 5. Print result in the format: “position – xxx, value of element – xxx”
-Sort and print collection 
- */
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
         int listSize = 10;
         Random rand = new Random();
+
         System.out.println("-----1-----");
-        List<Integer> myCollection = new ArrayList<Integer>();
-
+        List<Integer> myCollection = new LinkedList<>();
         for (int i = 0; i < listSize; i++) {
-            myCollection.add(rand.nextInt(10));
+            myCollection.add(rand.nextInt(25));
         }
-
         System.out.println(myCollection);
 
-        System.out.println();
         System.out.println("-----2-----");
-
-        List<Integer> newCollection = new LinkedList<Integer>();
+        List<Integer> newCollection = new ArrayList<>();
         for (int i = 0; i < myCollection.size(); i++) {
-
             if (myCollection.get(i) > 5) {
-                newCollection.add(myCollection.get(i));
+                newCollection.add(i);
             }
         }
         System.out.println(newCollection);
-        System.out.println();
+
         System.out.println("-----3-----");
-        for (int i = 0; i < myCollection.size(); i++) {
-            if (myCollection.get(i) > 5) {
-                myCollection.remove(i);
-                i--;
-            }
+        Iterator itr = myCollection.iterator();
+        while (itr.hasNext()) {
+            int x = (Integer) itr.next();
+            if (x > 20)
+                itr.remove();
         }
         System.out.println(myCollection);
-        System.out.println();
-        System.out.println("-----4-----");
 
+        System.out.println("-----4-----");
         myCollection.add(2, 1);
         myCollection.add(8, -3);
         myCollection.add(5, -4);
         System.out.println(myCollection);
 
-
+        System.out.println("-----5-----");
+        Collections.sort(myCollection);
+        System.out.println(myCollection);
     }
 }

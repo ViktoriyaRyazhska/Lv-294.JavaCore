4ed216d1c61fe1cd6ccd8ecfd5d0dc319574d5f0
Ira <ira.shturyn@gmail.com>
2018-02-21 15:43:31 +0200

    clw5

--- Classwork5/src/PracticalTask1/AppSecondVariant.java
+++ Classwork5/src/PracticalTask1/AppSecondVariant.java
@@ -5,18 +5,13 @@ import java.util.Random;
 
 public class AppSecondVariant {
     public static void main(String[] args) {
-        ArrayList<Integer> myCollection = getRandomNumbers(10);
-        int num;
+        System.out.println("-----1-----");
+        ArrayList<Integer> myCollection = new ArrayList<Integer>();
+        System.out.print(getRandomNumbers(myCollection, 10));
 
-        for (int i = 0; i < myCollection.size(); i++) {
-            num = myCollection.get(i);
-            System.out.println(num);
-        }
     }
 
-    private static ArrayList<Integer> getRandomNumbers(int sizeArray) {
-        ArrayList<Integer> addNumb = new ArrayList<Integer>();
-
+    public static ArrayList<Integer> getRandomNumbers(ArrayList<Integer> addNumb, int sizeArray) {
         Random rand = new Random();
         for (int i = 0; i < sizeArray; i++) {
             addNumb.add(rand.nextInt(10));
@@ -24,3 +19,12 @@ public class AppSecondVariant {
         return addNumb;
     }
 }
+// System.out.println("-----3-----");
+//           for (int i = 0; i < myCollection.size(); i++) {
+//        if (myCollection.get(i) > 20) {
+//        myCollection.remove(i);
+//        i--;
+//        }
+//        }
+// System.out.println("-----5-----");
+//         myCollection.sort(Comparator.naturalOrder());

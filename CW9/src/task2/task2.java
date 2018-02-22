d7c4cee71171cca0dd04a8911278b1f2b61414c6
vpodotc <vpodotc@class89.training.local>
2018-02-22 18:33:42 +0200

    CW9 task_2

---
+++ CW9/src/task2/task2.java
@@ -0,0 +1,19 @@
+//http://developer.alexanderklimov.ru/android/java/extends.php
+
+
+
+package task2;
+
+public class task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+	}
+
+}
